545f0aeb8dc2c9f8ddb7e9c6fa3b07a72ccfc8e4
Marvin Reyes <reyemarv@gmail.com>
2020-02-16 05:34:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,8 @@ class Song
   end 
   
   def self.find_or_create_by_name(name)
-    unless self.find_by_name(name) then self.create_by_name(name)
+    unless self.find_by_name(name) return name  
+    then self.create_by_name(name)
   end
 end 
 end
