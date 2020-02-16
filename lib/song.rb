a62bfbf89e62c9799ab04cb993deb76ef9d6dae4
Marvin Reyes <reyemarv@gmail.com>
2020-02-16 05:45:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,10 @@ class Song
   end
   
   def self.create_from_filename(name)
-    self.new_from_filename(name)
-    @@all<< song 
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song 
+    song 
   end 
 end
