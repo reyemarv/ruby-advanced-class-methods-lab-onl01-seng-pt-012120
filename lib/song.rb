1a3368d89abcb6c52dae26b0eee57f2eec4ce4d8
Marvin Reyes <reyemarv@gmail.com>
2020-02-16 05:42:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,4 +40,11 @@ class Song
   def self.alphabetical
     @@all.sort_by {|song| song.name} 
   end 
+  
+   def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
 end
